3af4301999a903dc389297bab8e4f5852e50cfaf
Ihor_Lysytsyn <kitskun@gmail.com>
2020-07-22 22:46:11 +0300

    Added basic tests

--- TestCafe/integration/alteryxTest.integration.js
+++ TestCafe/integration/alteryxTest.integration.js
@@ -1,4 +1,5 @@
 const pjson =  require("../../package.json");
+const { ClientFunction, Selector, after } = require('testcafe');
 const BASE_URL = pjson.baseUrl;
 const FIRST_URL = `${BASE_URL}/first`;
 const SECOND_URL = `${BASE_URL}/second`;
@@ -9,18 +10,34 @@ const GREEN_TEXT = 'Correct';
 const RED_COLOR = 'rgb(255, 0, 0)';
 const GREEN_COLOR = 'rgb(0, 255, 0)';
 
-  test( {
-      // check if url is correct. It means it's same as expected (first), no redirection happened after initial attempt to access
+const getLocation = ClientFunction(() => document.location.href);
 
-      // click on button and check if url is correct after redirection
+fixture('Getting Started').page(BASE_URL);
 
-      // check that h2 text is 'Incorrect' and color is red by default
+// It's too hard to find adequate name for this test :)
+test('Basic user journey', async t => { 
+  // Check if url correct without redirect
+  await t.expect(getLocation()).eql(FIRST_URL);
 
-      //type correct value and check that h2 text is 'Correct' and color is green
+  // Click on button and check if url correct after redirection
+  const redirectButton = Selector('button').withAttribute('try-to-find-this-attribute', 'find-by-me')
+  await t.click(redirectButton);
+  await t.expect(getLocation()).eql(SECOND_URL);
 
-      //type incorrect value and check that h2 text is 'Incorrect' and color is red
-  })
- 
-after({
-  clear input field
-})
+  const h2 = Selector('h2');
+
+  // check that h2 text is "Incorrect" and color is red by default
+  await t.expect(h2.withText(RED_TEXT).exists).ok()
+  await t.expect(h2.withText(RED_TEXT).getStyleProperty('color')).eql(RED_COLOR);
+
+  // type correct value and check that h2 text is "Correct" and color is green
+  const correctText = await Selector('h1').child('span').textContent;
+  await t.typeText('input', correctText);
+  await t.expect(h2.withText(GREEN_TEXT).exists).ok()
+  await t.expect(h2.withText(GREEN_TEXT).getStyleProperty('color')).eql(GREEN_COLOR);
+
+  // type correct value and check that h2 text is "Correct" and color is green
+  await t.typeText('input', `${correctText}_1`); // just to be sure this is incorrect value
+  await t.expect(h2.withText(RED_TEXT).exists).ok()
+  await t.expect(h2.withText(RED_TEXT).getStyleProperty('color')).eql(RED_COLOR);
+});
